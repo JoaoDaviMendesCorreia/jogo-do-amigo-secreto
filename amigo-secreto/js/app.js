0ee818dca883808522e777a15d27fba22cb45ec1
João Davi Mendes Correia <168194655+JoaoDaviMendesCorreia@users.noreply.github.com>
2024-05-16 09:21:14 -0300

    boas práticas

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,38 +1,37 @@
-
-function adicionar (){
-    let adicionarAmigo = document.getElementById ('nome-amigo').value
-   let amigosIncluido = document.getElementById ('lista-amigos')
-    if (adicionarAmigo == ''){
+function adicionar() {
+    let adicionarAmigo = document.getElementById('nome-amigo').value
+    let amigosIncluido = document.getElementById('lista-amigos')
+    if (adicionarAmigo == '') {
         alert('adicione algo a lista')
         return
-        }else if(listaAmigo.includes(adicionarAmigo)){
-            alert ('mesmo nome')
-            return
-    }else{amigosIncluido.textContent = `${adicionarAmigo}, ${listaAmigo.join(', ')}            `
- listaAmigo.push (adicionarAmigo)}
+    } else if (listaAmigo.includes(adicionarAmigo)) {
+        alert('mesmo nome')
+        return
+    } else {
+        amigosIncluido.textContent = `${adicionarAmigo}, ${listaAmigo.join(', ')}            `
+        listaAmigo.push(adicionarAmigo)
+    }
 
 }
- listaAmigo = []
+listaAmigo = []
 
 function sortear() {
-if (listaAmigo.length <= 3){
-    alert ('adicione pelo menos 4 amigos')
-    return
-}
+    if (listaAmigo.length <= 3) {
+        alert('adicione pelo menos 4 amigos')
+        return
+    }
 
     embaralha(listaAmigo);
-let sorteio = document.getElementById('lista-sorteio');
+    let sorteio = document.getElementById('lista-sorteio');
 
-for (let i = 0; i < listaAmigo.length; i++) {
-   if (i == listaAmigo.length - 1) {
-    sorteio.innerHTML = sorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[0] + '<br>';
- } else {
-  sorteio.innerHTML = sorteio.innerHTML +listaAmigo[i] + ' --> ' + listaAmigo[i + 1] + '<br>';
- }
- }
+    for (let i = 0; i < listaAmigo.length; i++) {
+        if (i == listaAmigo.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[i + 1] + '<br>';
+        }
+    }
 }
-
-
 
 
 
@@ -41,10 +40,10 @@ function embaralha(lista) {
     for (let indice = lista.length; indice; indice--) {
         const indiceAleatorio = Math.floor(Math.random() * indice);
         // atribuição via destructuring
-        [lista[indice - 1], lista[indiceAleatorio]] = 
-            [lista[indiceAleatorio], lista[indice - 1]];
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
+
 function reiniciar() {
     listaAmigo = [];
     document.getElementById('lista-amigos').innerHTML = '';
