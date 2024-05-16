4f3f3d0bb88097d53119ef2b212947921c5ff661
João Davi Mendes Correia <168194655+JoaoDaviMendesCorreia@users.noreply.github.com>
2024-05-16 08:42:31 -0300

    Add files via upload

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,52 @@
+
+function adicionar (){
+    let adicionarAmigo = document.getElementById ('nome-amigo').value
+   let amigosIncluido = document.getElementById ('lista-amigos')
+    if (adicionarAmigo == ''){
+        alert('adicione algo a lista')
+        return
+        }else if(listaAmigo.includes(adicionarAmigo)){
+            alert ('mesmo nome')
+            return
+    }else{amigosIncluido.textContent = `${adicionarAmigo}, ${listaAmigo.join(', ')}            `
+ listaAmigo.push (adicionarAmigo)}
+
+}
+ listaAmigo = []
+
+function sortear() {
+if (listaAmigo.length <= 3){
+    alert ('adicione pelo menos 4 amigos')
+    return
+}
+
+    embaralha(listaAmigo);
+let sorteio = document.getElementById('lista-sorteio');
+
+for (let i = 0; i < listaAmigo.length; i++) {
+   if (i == listaAmigo.length - 1) {
+    sorteio.innerHTML = sorteio.innerHTML + listaAmigo[i] + ' --> ' + listaAmigo[0] + '<br>';
+ } else {
+  sorteio.innerHTML = sorteio.innerHTML +listaAmigo[i] + ' --> ' + listaAmigo[i + 1] + '<br>';
+ }
+ }
+}
+
+
+
+
+
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+function reiniciar() {
+    listaAmigo = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
